05678d927c7baf735fca3536831906df478ef01c
dnjzm108 <dnjzm108@kakao.com>
2021-05-26 17:17:07 +0900

    api

---
+++ nodejs/api/server.js
@@ -0,0 +1,20 @@
+/*API 애플리케이션 프로그래밍 인터페이스
+       특정파일     개발       보조장치 (마우스or키보드 같이)
+       */
+
+const express = require('express');
+const nunjucks = require('nunjucks');
+const app = express();
+
+app.set('view engine','html');
+nunjucks.configure('views',{
+    express:app
+})
+
+app.get('/',(req,res)=>{
+    res.render('test')
+})
+
+app.listen(3000,()=>{
+    console.log('server start port : 3000');
+})
